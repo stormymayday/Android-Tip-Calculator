d6e53bcefb5f5348fa85d3594307bea1f0105a9a
stormymayday <stormymayday7@gmail.com>
2024-05-21 20:26:54 -0700

    Created Instance Variables, References, and Listeners

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.tipcalculator;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextView.OnEditorActionListener, View.OnClickListener {
+
+    // Instance Variables
+    private EditText billAmountEditText;
+    private TextView percentTextView;
+    private Button percentUpButton;
+    private Button percentDownButton;
+    private TextView tipTextView;
+    private TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Getting references to the UI Controls
+        billAmountEditText = (EditText) findViewById(R.id.billAmountEditText);
+        percentTextView = findViewById(R.id.percentTextView);
+        percentUpButton = findViewById(R.id.percentUpButton);
+        percentDownButton = findViewById(R.id.percentDownButton);
+        tipTextView = findViewById(R.id.tipTextView);
+        totalTextView = findViewById(R.id.totalTextView);
+
+        // Listeners
+        billAmountEditText.setOnEditorActionListener(this);
+        percentDownButton.setOnClickListener(this);
+        percentUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        return false;
     }
 }
